f6ac9715e8ce6b4332cf480c096ea813a3ed3846
Pravesh Kumar <31381683+Pravesh701@users.noreply.github.com>
2020-04-28 13:03:24 +0530

    Added stack for javascript

--- JavaScript/Stack_In_JavaSCript.js
+++ JavaScript/Stack_In_JavaSCript.js
@@ -16,3 +16,32 @@ const buildStack = () => {
     size,
   };
 };
+/*
+*Because we created the items stack inside our buildStack function, it is "private". It can be accessed only within the function. In this case, only push, pop, and so one could touch the data. This is exactly what we're looking for.
+*/
+//And how do we use it? Like this:
+
+const stack = buildStack();
+
+stack.isEmpty(); // true
+
+stack.push(1); // [1]
+stack.push(2); // [2, 1]
+stack.push(3); // [3, 2, 1]
+stack.push(4); // [4, 3, 2, 1]
+stack.push(5); // [5, 4, 3, 2, 1]
+
+stack.peek(); // 5
+stack.size(); // 5
+stack.isEmpty(); // false
+
+stack.pop(); // [4, 3, 2, 1]
+stack.pop(); // [3, 2, 1]
+stack.pop(); // [2, 1]
+stack.pop(); // [1]
+
+stack.isEmpty(); // false
+stack.peek(); // 1
+stack.pop(); // []
+stack.isEmpty(); // true
+stack.size(); // 0
